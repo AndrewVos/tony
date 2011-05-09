8574b1696705fa2b1d05437b57f154458c32a1e9
Andrew Vos <andrew.vos@gmail.com>
2011-05-09 23:29:03 +0100

    added an rspec helper file

--- lib/tony/generators/rspec/rspec_generator.rb
+++ lib/tony/generators/rspec/rspec_generator.rb
@@ -2,6 +2,7 @@ Tony::generators << Tony::Generator.new do |options|
   options.name = "rspec"
   options.description = "Generates rspec rake tasks, spec directories and a spec_helper"
   options.files = {
-    'Rakefile' => File.read(File.join(File.dirname(__FILE__), 'Rakefile'))
+    'Rakefile'       => File.read(File.join(File.dirname(__FILE__), 'Rakefile')),
+    'spec/helper.rb' => File.read(File.join(File.dirname(__FILE__), 'spec/helper.rb'))
   }
 end
