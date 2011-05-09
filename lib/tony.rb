7cd7060fca9b92bdf34586f856c435e51d166589
Andrew Vos <andrew.vos@gmail.com>
2011-05-09 21:53:44 +0100

    changed the generators file structure

--- lib/tony.rb
+++ lib/tony.rb
@@ -1,3 +1,3 @@
 require_relative 'tony/tony'
 require_relative 'tony/generator'
-require_relative 'tony/generators/rspec_generator'
+require_relative 'tony/generators/rspec/rspec_generator'
--- lib/tony/generators/rspec/rspec_generator.rb
+++ lib/tony/generators/rspec/rspec_generator.rb
@@ -2,13 +2,6 @@ Tony::generators << Tony::Generator.new do |options|
   options.name = "rspec"
   options.description = "Generates rspec rake tasks, spec directories and a spec_helper"
   options.files = {
-    'Rakefile' => <<-FILE
-require 'rspec/core/rake_task'
-
-desc "Run specs"
-RSpec::Core::RakeTask.new do |t|
-  t.pattern = "./spec/**/*_spec.rb" # don't need this, it's default.
-end
-    FILE
+    'Rakefile' => File.read(File.join(File.dirname(__FILE__), 'Rakefile'))
   }
 end
